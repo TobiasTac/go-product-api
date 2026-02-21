1f38d3953c5d677fe84025ba6df9c5c2b90d1531
TobiasTac <tobias.vida@live.com>
2026-02-21 14:10:24 -0300

    feat: error handling when creating products

--- internal/entity/product.go
+++ internal/entity/product.go
@@ -1,7 +1,17 @@
 package entity
 
 import (
+	"errors"
+
 	"github.com/TobiasTac/go-product-api/pkg/entity"
+)
+
+var (
+	ErrIDIsRequired    = errors.New("id is required")
+	ErrInvalidID       = errors.New("invalid id")
+	ErrNameIsRequired  = errors.New("name is required")
+	ErrPriceIsRequired = errors.New("price is required")
+	ErrInvalidPrice    = errors.New("invalid price")
 )
 
 type Product struct {
@@ -9,4 +19,24 @@ type Product struct {
 	Name      string    `json:"name"`
 	Price     float64   `json:"price"`
 	CreatedAt string    `json:"created_at"`
+}
+
+func (p *Product) Validate() error {
+	if p.ID.String() == "" {
+		return ErrIDIsRequired
+	}
+	if _, err := entity.ParseID(p.ID.String()); err != nil {
+		return ErrInvalidID
+	}
+	if p.Name == "" {
+		return ErrNameIsRequired
+	}
+	if p.Price == 0 {
+		return ErrPriceIsRequired
+	}
+	if p.Price < 0 {
+		return ErrInvalidPrice
+	}
+
+	return nil
 }
