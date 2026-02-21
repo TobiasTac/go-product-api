d49dd5623d04cdb50606d69ada4a9e5f6fa2bdcd
TobiasTac <tobias.vida@live.com>
2026-02-21 13:39:36 -0300

    test: user validate password

--- internal/entity/user_test.go
+++ internal/entity/user_test.go
@@ -15,3 +15,10 @@ func TestNewUser(t *testing.T) {
 	assert.Equal(t, "Tobias Alves", user.Name)
 	assert.Equal(t, "tobias.vida@live.com", user.Email)
 }
+func TestUser_ValidatePassword(t *testing.T) {
+	user, err := NewUser("Tobias Alves", "tobias.vida@live.com", "123123")
+	assert.Nil(t, err)
+	assert.True(t, user.ValidatePassword("123123"))
+	assert.False(t, user.ValidatePassword("123456"))
+	assert.NotEqual(t, "123123", user.Password)
+}
