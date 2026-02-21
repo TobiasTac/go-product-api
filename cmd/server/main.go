3c0832b1e256a20f841d8937bc40dc23b63e7580
TobiasTac <tobias.vida@live.com>
2026-02-21 13:01:06 -0300

    chore: initial config setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,11 @@
 package main
 
+import "github.com/TobiasTac/go-product-api/configs"
+
 func main() {
-	//
+	config, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	println(config.DBDriver)
 }
