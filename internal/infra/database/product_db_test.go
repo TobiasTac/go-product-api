dcb07c60dd105719e57be50ad69619c14e0888d6
TobiasTac <tobias.vida@live.com>
2026-02-22 20:09:33 -0300

    test: create new product

---
+++ internal/infra/database/product_db_test.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/TobiasTac/go-product-api/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateNewProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product 1", 20.00)
+	assert.NoError(t, err)
+	productDB := NewProduct(db)
+	err = productDB.Create(product)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, product.ID)
+}
