d5ef2dfb9f573fc61c7e46f317286c44de503ad3
TobiasTac <tobias.vida@live.com>
2026-02-21 21:07:09 -0300

    feat: create user service

--- internal/infra/database/user_db.go
+++ internal/infra/database/user_db.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"github.com/TobiasTac/go-product-api/internal/entity"
 	"gorm.io/gorm"
 )
 
@@ -10,4 +11,8 @@ type User struct {
 
 func NewUser(db *gorm.DB) *User {
 	return &User{DB: db}
+}
+
+func (u *User) Create(user *entity.User) error {
+	return u.DB.Create(user).Error
 }
