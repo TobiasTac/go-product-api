a629e5f569228ebbe228722031592fc6c26d23c4
TobiasTac <tobias.vida@live.com>
2026-02-23 23:40:29 -0300

    feat: get product handler

--- internal/infra/webserver/handlers/product_handlers.go
+++ internal/infra/webserver/handlers/product_handlers.go
@@ -7,6 +7,7 @@ import (
 	"github.com/TobiasTac/go-product-api/internal/dto"
 	"github.com/TobiasTac/go-product-api/internal/entity"
 	"github.com/TobiasTac/go-product-api/internal/infra/database"
+	"github.com/go-chi/chi"
 )
 
 type ProductHandler struct {
@@ -40,4 +41,21 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product, err := h.ProductDB.FindById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(product)
 }
