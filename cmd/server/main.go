3d264f3a6d8d5454cb8e6ae1c1fad789945433b1
TobiasTac <tobias.vida@live.com>
2026-02-24 11:49:23 -0300

    feat: create user handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -22,8 +22,12 @@ func main() {
 		panic(err)
 	}
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
+
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
 
 	r := chi.NewRouter()
 	r.Post("/products", productHandler.CreateProduct)
@@ -31,6 +35,8 @@ func main() {
 	r.Get("/products/{id}", productHandler.GetProduct)
 	r.Put("/products/{id}", productHandler.UpdateProduct)
 	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	r.Post("/users", userHandler.CreateUser)
 
 	http.ListenAndServe(":8000", r)
 }
--- internal/infra/webserver/handlers/user_handlers.go
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "github.com/TobiasTac/go-product-api/internal/infra/database"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/TobiasTac/go-product-api/internal/dto"
+	"github.com/TobiasTac/go-product-api/internal/entity"
+	"github.com/TobiasTac/go-product-api/internal/infra/database"
+)
 
 type UserHandler struct {
 	UserDB database.UserInterface
@@ -8,4 +15,28 @@ type UserHandler struct {
 
 func NewUserHandler(userDB database.UserInterface) *UserHandler {
 	return &UserHandler{UserDB: userDB}
+}
+
+func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserInput
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	u, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
