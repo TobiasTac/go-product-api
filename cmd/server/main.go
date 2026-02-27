83d29c9fa59a39aa24c0e01442eada6eac87cc92
TobiasTac <tobias.vida@live.com>
2026-02-27 20:54:09 -0300

    feat: swagger handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,16 +4,35 @@ import (
 	"net/http"
 
 	"github.com/TobiasTac/go-product-api/configs"
+	_ "github.com/TobiasTac/go-product-api/docs"
 	"github.com/TobiasTac/go-product-api/internal/entity"
 	"github.com/TobiasTac/go-product-api/internal/infra/database"
 	"github.com/TobiasTac/go-product-api/internal/infra/webserver/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+// @title           Go Product API
+// @version         1.0
+// @description     Product API with auhtentication
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Tobias Alves
+// @contact.url    https://github.com/TobiasTac
+// @contact.email  tobias.vida@live.com
+
+// @license.name   TAC License
+// @license.url    https://github.com/TobiasTac/go-product-api
+
+// @host      localhost:8000
+// @BasePath  /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
 	configs, err := configs.LoadConfig(".")
 	if err != nil {
@@ -49,6 +68,8 @@ func main() {
 
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
 
 	http.ListenAndServe(":8000", r)
 }
